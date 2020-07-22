6b86cb0aa54567fe58317d2a012a3a72649bcdee
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-07-22 17:41:01 +0900

    add command-line args to output debug messages

--- src/main.py
+++ src/main.py
@@ -3,12 +3,19 @@
 import logging
 from typing import Final
 
+import click
+
 import sample_module
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option("--debug", is_flag=True, help="Output debug messages.")
+def _main(debug):
     # log設定
     LOG_FORMAT: Final = "%(asctime)s [%(name)s:%(levelname)s] %(message)s"
     logging.basicConfig(format=LOG_FORMAT)
+    if debug:
+        logging.getLogger().setLevel("DEBUG")
     logger = logging.getLogger(__name__)
 
     # 以下，メイン関数の処理
@@ -25,3 +32,7 @@ if __name__ == "__main__":
     sample_module.logger.setLevel("DEBUG")
     sample_instance = sample_module.SampleClass()
     sample_instance.sample_func()
+
+
+if __name__ == "__main__":
+    _main()
