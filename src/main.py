951af64d6ad47d61b050fecdcc90ed8b35fb7bf3
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2020-07-22 17:11:24 +0900

    add sample scripts for logging: #2 (#6)
    * add sample scripts for logging: #2

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+import logging
+from typing import Final
+
+import sample_module
+
+if __name__ == "__main__":
+    # log設定
+    LOG_FORMAT: Final = "%(asctime)s [%(name)s:%(levelname)s] %(message)s"
+    logging.basicConfig(format=LOG_FORMAT)
+    logger = logging.getLogger(__name__)
+
+    # 以下，メイン関数の処理
+    logger.debug("debug message")
+    logger.info("information message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
+
+    # log_levelがWARNINGのため下記関数を呼んだ際のログは出力されない
+    sample_module.sample_func()
+
+    # モジュール単位でlog_levelを下げることでログが出力されるようになる
+    sample_module.logger.setLevel("DEBUG")
+    sample_instance = sample_module.SampleClass()
+    sample_instance.sample_func()
